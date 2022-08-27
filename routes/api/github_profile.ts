b0980b2477caeeb44ffcf92f81b8951832c94c68
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2022-08-28 00:48:43 +0900

    use github api

---
+++ routes/api/github_profile.ts
@@ -0,0 +1,19 @@
+/** @jsx h */
+import { Handlers } from "$fresh/server.ts";
+
+export interface User {
+  login: string;
+  name: string;
+  avatar_url: string;
+}
+
+export const handler: Handlers<User | null> = {
+  async GET(_, ctx) {
+    const resp = await fetch(`https://api.github.com/users/mitubaEX`);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const user: User = await resp.json();
+    return ctx.render(user);
+  },
+};
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,8 +2,30 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export interface User {
+  login: string;
+  name: string;
+  avatar_url: string;
+}
+
+export const handler: Handlers<User | null> = {
+  async GET(_, ctx) {
+    const resp = await fetch(`https://api.github.com/users/mitubaEX`);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const user: User = await resp.json();
+    return ctx.render(user);
+  },
+};
+
+export default function Home({ data }: PageProps<User | null>) {
+  if (!data) {
+    return <h1>User not found</h1>;
+  }
+
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
@@ -16,6 +38,9 @@ export default function Home() {
         file, and refresh.
       </p>
       <Counter start={3} />
+      <img src={data.avatar_url} width={64} height={64} />
+      <h1>{data.name}</h1>
+      <p>{data.login}</p>
     </div>
   );
 }
